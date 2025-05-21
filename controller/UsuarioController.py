3e84a7d9af3c52781a0a4a929797852d7d6447da
mateusvieiraifce <101875553+mateusvieiraifce@users.noreply.github.com>
2025-05-21 15:19:09 -0300

    subindo correçoes

--- controller/UsuarioController.py
+++ controller/UsuarioController.py
@@ -30,3 +30,46 @@ def create():
     db.commit()
 
     return jsonify({'msg':'Salvo com sucesso'}), 200
+
+
+@app.route("/usuarios/<int:id>", methods = ["GET"])
+def get_usuarios(id):
+    db = Sessionlocal()
+    usuario = db.query(Usuario).get(id)
+    if (usuario):
+        return jsonify(usuario.to_dict()), 200
+    else:
+        return jsonify({'msg':'Usuário não encontrado'}), 404
+
+@app.route("/usuarios/<int:id>", methods = ["DELETE"])
+def delete_usuarios(id):
+    db = Sessionlocal()
+    usuario = db.query(Usuario).get(id)
+    if (usuario):
+        db.delete(usuario)
+        db.commit()
+        return jsonify({'msg':'Usuario apagado'}), 204
+    else:
+        return jsonify({'msg':'Usuário não encontrado'}), 404
+
+
+@app.route("/usuarios", methods = ["POST"])
+def create_usuarios():
+    db = Sessionlocal()
+    data = request.get_json()
+    usuario = Usuario(nome = data['nome'], data = data['aniversario'])
+    db.add(usuario)
+    db.commit()
+
+    return jsonify({'msg':'Usuario criado com sucesso!', 'usu':usuario.to_dict()}), 201
+
+@app.route("/usuarios/<int:id>", methods = ["PUT"])
+def update_usuarios(id):
+    db = Sessionlocal()
+    ub = db.query(Usuario).get(id)
+    data = request.get_json()
+    ub.nome = data['nome']
+    ub.data = data['aniversario']
+    db.commit()
+
+    return jsonify({'msg':'Usuario atualizado com sucesso!', 'usu':ub.to_dict()}), 200
